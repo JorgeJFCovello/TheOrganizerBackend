0e058095dde0a99e21255648506007e816656aa0
JorgeJFCovello <jorgejfcovello@gmail.com>
2021-12-04 19:24:46 -0300

    User update method added

--- controllers/users/user.controller.js
+++ controllers/users/user.controller.js
@@ -26,13 +26,25 @@ const createUser = async(req, resp) => {
         await usuario.save()
         resp.status(200).json({msg: 'User created successfully'})
 }
+const updateUser = async(req, resp) => {
 
-getEncryptedPassword = (password) => {
+    const { id } = req.params;
+    const {google, password, ... others} = req.body;
+    const usuario = await Usuario.findByIdAndUpdate(id, others);
+    if (password) {
+        usuario.password = getEncryptedPassword(password)
+    }
+    await usuario.save()
+    resp.status(200).json({msg: 'User updated successfully'})
+}
+
+const getEncryptedPassword = (password) => {
     const salt = bcrypt.genSaltSync()
     return bcrypt.hashSync(password,salt)
 }
 
 module.exports = {
     authenticate,
-    createUser
+    createUser,
+    updateUser
 }
--- routes/user/user.js
+++ routes/user/user.js
@@ -5,15 +5,23 @@ const router = Router()
 const {
     authenticate,
     createUser,
+    updateUser
 } = require('../../controllers/users/user.controller')
 
-router.post('/authenticate',authenticate)
+router.post('/authenticate', authenticate)
 router.post('/user', [
     check('email', 'Invalidate email.').isEmail(),
     check('username', 'Name must not be empty').not().isEmpty(),
     check('password', 'Password must not be empty').not().isEmpty(),
     validateFields
 ], createUser)
+
+router.put('/user/:id', [
+    check('email', 'Invalidate email.').isEmail(),
+    check('username', 'Name must not be empty').not().isEmpty(),
+    check('password', 'Password must not be empty').not().isEmpty(),
+    validateFields
+], updateUser)
 
 
 module.exports = router
